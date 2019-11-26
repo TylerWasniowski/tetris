aef6182b36e9560e6ba7f07290ff3ee6583b9964
TylerWasniowski <TylerWasniowski@gmail.com>
2019-11-26 13:57:43 -0800

    Added ai.py, observations.pi, and a concatenated observations file

---
+++ boost/ai.py
@@ -0,0 +1,58 @@
+from hmmlearn import hmm
+import math
+import os.path
+import pickle
+
+
+HMM_FILE_NAME_BASE = "hmm_"
+HMM_FILE_NAME_EXTENSION = ".hmm"
+
+
+def load_hmm(n=8):
+    file_name = __get_file_name(n)
+    if not os.path.exists(file_name):
+        return None
+
+    with open(file_name, "rb") as file:
+        return pickle.load(file)
+
+
+# Saves hmm if it scores better on observations than already saved hmm
+def maybe_save_hmm(model, observations):
+    n = model.n_components
+    saved_model = load_hmm(n)
+    if saved_model:
+        saved_model_score = saved_model.score(observations)
+        model_score = model.score(observations)
+
+        if saved_model_score >= model_score:
+            return
+
+    print("Saving hmm")
+    file_name = __get_file_name(n)
+    with open(file_name, "wb") as file:
+        pickle.dump(model, file)
+
+
+def train_hmm(observations, n=8, n_iter=350, restarts=1):
+    best_model = None
+    best_score = -math.inf
+    for i in range(1, restarts + 1):
+        model = hmm.MultinomialHMM(n_components=n, n_iter=n_iter, tol=0.0001)
+        model.fit(observations)
+        score = model.score(observations)
+        if score > best_score:
+            print("Found model with better score:", score, "(+", str(score - best_score) + ")")
+            best_model = model
+            best_score = score
+            maybe_save_hmm(best_model, observations)
+        print("Finished restart:", i)
+        print("Score this restart:", score)
+        print("Best score so far:", best_score)
+        print()
+
+    return best_model
+
+
+def __get_file_name(n):
+    return HMM_FILE_NAME_BASE + str(n) + HMM_FILE_NAME_EXTENSION
---
+++ boost/observations.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+BOARD_SIZE = 20, 10
+
+
+def raw(file_name="../full_observations_A=0.0_U=20.0.txt", start_char='A', end_char='Z'):
+    with open(file_name, "r") as file:
+        # Filters out EOF newline and anything else that's not an observation
+        observations = list(filter(lambda ch: start_char <= ch <= end_char, file.read()))
+        symbols = np.unique(observations)
+        # Map each symbol to a number in [0, 1, ..., m - 1] where is the number of symbols
+        observations = list(map(lambda ch: symbols.searchsorted(ch), observations))
+        # Put it in format for hmm library [[obs1], [obs2], ...]
+        return np.array([observations]).T
+
+
+def compressed(raw_obs=None):
+    if not raw_obs:
+        raw_obs = raw().T[0]
+
+    compressed_obs = []
+
+    symbols = np.unique(raw_obs)
+    for start_index in range(0, len(raw_obs), BOARD_SIZE[1]):
+        end_index = start_index + BOARD_SIZE[1]
+        board = raw_obs[start_index:end_index]
+        min_obs = min(board)
+        for i in range(0, len(board)):
+            # This represents a column with no blocks in it
+            if board[i] == symbols[-1]:
+                # Give this column a unique symbol
+                board[i] = 0
+            else:
+                board[i] -= min_obs - 1
+        compressed_obs.extend(board)
+
+    return np.array([compressed_obs]).T
+
+
+# Returns an array of 10 numbers, each number represents the location of the highest occupied block in each column
+def from_board(board, start_row=0):
+    obs = np.zeros(BOARD_SIZE[1])
+    for col in range(0, BOARD_SIZE[1]):
+        obs[col] = BOARD_SIZE[0]
+        for row in range(start_row, BOARD_SIZE[0]):
+            if board[row][col]:
+                obs[col] = row
+                break
+
+    return obs
