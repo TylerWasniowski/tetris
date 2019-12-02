4814a4d36001e32b77a25504941b84ca70b76953
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-01 23:01:43 -0800

    Fixed scores

--- boost/tetris_boost.cpp
+++ boost/tetris_boost.cpp
@@ -16,7 +16,7 @@ const int ROTATIONS = 4;
 const int NUM_SHAPES = 8;
 
 // Scores for 0, 1, 2, ..., n line clears with a single placement
-const array<int, PIECE_SIZE> SCORES = {{0, 40, 100, 300, 1200}};
+const array<int, 5> SCORES = {{0, 40, 100, 300, 1200}};
 
 const array<array<bool, PIECE_SIZE>, PIECE_SIZE> BLANK_SHAPE = {
     {{false, false, false, false},
