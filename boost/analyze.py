946212d7ffbcaabb835b1265e319eb38a5edb673
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-03 10:10:40 -0800

    added analyze

---
+++ boost/analyze.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+for n in range(2, 21):
+    try:
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.mean(a[0])))
+    finally:
+        print()
+
+for n in range(2, 21):
+    try:
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.max(a[0])))
+    finally:
+        print()
+
+for n in range(2, 21):
+    try:
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.std(a[0])))
+    finally:
+        print()
