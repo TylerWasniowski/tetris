8691395d40090a95dbbf41a614e95d8d141361c4
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-03 10:13:38 -0800

    fixed

--- boost/analyze.py
+++ boost/analyze.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 for n in range(2, 21):
-    try:
-        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.mean(a[0])))
-    finally:
-        print()
+    if not n == 11:
+        try:
+            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+            print(str(n) + "                            " + str(np.mean(a[0])))
+        finally:
+            print()
 
 for n in range(2, 21):
-    try:
-        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.max(a[0])))
-    finally:
-        print()
+    if not n == 11:
+        try:
+            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+            print(str(n) + "                            " + str(np.max(a[0])))
+        finally:
+            print()
 
 for n in range(2, 21):
-    try:
-        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.std(a[0])))
-    finally:
-        print()
+    if not n == 11:
+        try:
+            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+            print(str(n) + "                            " + str(np.std(a[0])))
+        finally:
+            print()
