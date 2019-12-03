42cc9dc079c780bb70091d84c0867eede8f926b6
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-03 10:59:26 -0800

    Changed data output

--- boost/analyze.py
+++ boost/analyze.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-for n in range(2, 21):
+for n in range(2, 22):
+    print("Mean scores")
     if not n == 11 and not n == 14:
         try:
             a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
@@ -8,18 +9,31 @@ for n in range(2, 21):
         finally:
             print()
 
-for n in range(2, 21):
-    if not n == 11 and not n == 14:
-        try:
-            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-            print(str(n) + "                            " + str(np.max(a[0])))
-        finally:
-            print()
-
-for n in range(2, 21):
+for n in range(2, 22):
+    print("Std deviation scores")
     if not n == 11 and not n == 14:
         try:
             a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
             print(str(n) + "                            " + str(np.std(a[0])))
         finally:
             print()
+
+for n in range(2, 22):
+    print("Mean number of moves")
+    if not n == 11 and not n == 14:
+        try:
+            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+            print(str(n) + "                            " + str(np.mean(a[1])))
+        finally:
+            print()
+
+for n in range(2, 22):
+    print("Std deviation number of moves")
+    if not n == 11 and not n == 14:
+        try:
+            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+            print(str(n) + "                            " + str(np.std(a[1])))
+        finally:
+            print()
+
+
