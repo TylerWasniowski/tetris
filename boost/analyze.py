36316748a585bfb6b6a88d3a1513b7b46770fd4c
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-03 11:03:18 -0800

    Removed newline

--- boost/analyze.py
+++ boost/analyze.py
@@ -3,37 +3,23 @@ import numpy as np
 print("Mean scores")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
-        try:
-            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-            print(str(n) + "                            " + str(np.mean(a[0])))
-        finally:
-            print()
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.mean(a[0])))
 
 print("Std deviation scores")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
-        try:
-            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-            print(str(n) + "                            " + str(np.std(a[0])))
-        finally:
-            print()
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.std(a[0])))
 
 print("Mean number of moves")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
-        try:
-            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-            print(str(n) + "                            " + str(np.mean(a[1])))
-        finally:
-            print()
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.mean(a[1])))
 
 print("Std deviation number of moves")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
-        try:
-            a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-            print(str(n) + "                            " + str(np.std(a[1])))
-        finally:
-            print()
-
-
+        a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
+        print(str(n) + "                            " + str(np.std(a[1])))
