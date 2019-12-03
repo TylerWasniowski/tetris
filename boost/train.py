8fa2e954bdb83a6cdacc5c011718b2526e54b09d
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-02 23:06:46 -0800

    Added raw option for training

--- boost/train.py
+++ boost/train.py
@@ -5,11 +5,17 @@ import observations
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--compressed", "-c", help="Whether to use compressed observations or not", default=True, type=bool)
 parser.add_argument("--n", "-n", help="Number of hidden states", default=8, type=int)
 parser.add_argument("--iters", "-i", help="Iterations to do with each set of initial conditions", default=350, type=int)
 parser.add_argument("--restarts", "-r", help="Times to restart with new initial conditions", default=1, type=int)
 parser.add_argument("--verbose", "-v", help="Print score each iter", default=False, type=bool)
 args = parser.parse_args()
 
-obs = observations.to_training_input(observations.compressed())
+if args.compressed:
+    obs = observations.compressed()
+else:
+    obs = observations.raw()
+    
+obs = observations.to_training_input(obs)
 ai.train_hmm(obs, n=args.n, n_iter=args.iters, restarts=args.restarts, verbose=args.verbose)
