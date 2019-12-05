458fa28ae617afacd22bb4df35af46f6ab027463
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-04 21:11:56 -0800

    Added length of array to analyze.py

--- boost/analyze.py
+++ boost/analyze.py
@@ -4,22 +4,22 @@ print("Mean scores")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.mean(a[0])))
+        print(str(len(a[0]) + "               " + str(n) + "              " + str(np.mean(a[0])))
 
 print("Std deviation scores")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.std(a[0])))
+        print(str(len(a[0]) + "               " + str(n) + "              " + str(np.std(a[0])))
 
 print("Mean number of moves")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.mean(a[1])))
+        print(str(len(a[0]) + "               " + str(n) + "              " + str(np.mean(a[1])))
 
 print("Std deviation number of moves")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
-        print(str(n) + "                            " + str(np.std(a[1])))
+        print(str(len(a[0]) + "               " + str(n) + "              " + str(np.std(a[1])))
