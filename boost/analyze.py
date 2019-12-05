c70a6e0e955fbcd285dfe38ed3c58f82d638df1c
TylerWasniowski <TylerWasniowski@gmail.com>
2019-12-05 01:44:20 -0800

    Fixed parens

--- boost/analyze.py
+++ boost/analyze.py
@@ -29,25 +29,25 @@ for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
         print(str(len(a[0])) + "               " + str(n) + "              " +
-              str(np.mean(np.array(list(map(lambda x: list(x), a[2]))[:, 0]))))
+              str(np.mean(np.array(list(map(lambda x: list(x), a[2])))[:, 0])))
 
 print("Mean of double line clears")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
         print(str(len(a[0])) + "               " + str(n) + "              " +
-              str(np.mean(np.array(list(map(lambda x: list(x), a[2]))[:, 1]))))
+              str(np.mean(np.array(list(map(lambda x: list(x), a[2])))[:, 1])))
 
 print("Mean of triple line clears")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
         print(str(len(a[0])) + "               " + str(n) + "              " +
-              str(np.mean(np.array(list(map(lambda x: list(x), a[2]))[:, 2]))))
+              str(np.mean(np.array(list(map(lambda x: list(x), a[2])))[:, 2])))
 
 print("Mean of quadruple line clears")
 for n in range(2, 22):
     if not n == 11 and not n == 14:
         a = np.load("hmm_stats_1-" + str(n) + ".npy", allow_pickle=True)
         print(str(len(a[0])) + "               " + str(n) + "              " +
-              str(np.mean(np.array(list(map(lambda x: list(x), a[2]))[:, 3]))))
+              str(np.mean(np.array(list(map(lambda x: list(x), a[2])))[:, 3])))
